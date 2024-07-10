9e9f34b323f56050a0dccd7bb0e7874e5dc44284
Adel <576853-astran@users.noreply.gitlab.com>
2024-07-10 23:59:33 +0200

    wip

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,17 +1,19 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.*
 import diceroller.composeapp.generated.resources.Res
 import diceroller.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
 @Composable
@@ -20,6 +22,8 @@ fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         var (selectedDice ,updateDice) = remember { mutableStateOf(Dice.SIX) }
+        var nbRolls by remember { mutableStateOf(1) }
+        var rolls by remember { mutableStateOf(listOf<Int>()) }
         a()
 
         PermanentNavigationDrawer(
@@ -31,6 +35,23 @@ fun App() {
 //                        Button(onClick = { println("machin2") }) { Text("truc2") }
 //                        Button(onClick = { println("machin3") }) { Text("truc3") }
                         DiceSelector(selectedDice ,updateDice)
+
+                        TextField(
+                            value = nbRolls.toString(),
+                            onValueChange = {
+                                if (it.isBlank()) {
+                                    nbRolls = 1
+                                } else {
+                                    val toIntOrNull = it.toIntOrNull()
+                                    if (toIntOrNull != null) {
+                                        nbRolls = toIntOrNull
+                                    }
+                                }
+
+                            },
+                            label = { Text("Number of Rolls") }
+                        )
+                        Button(onClick = {rolls = selectedDice.roll(nbRolls)  }) { Text("Roll") }
                     }
                 }
             },
@@ -40,6 +61,7 @@ fun App() {
 
                     Text("Selected $selectedDice")
                 }
+                RollResult(rolls)
                 AnimatedVisibility(showContent) {
                     val greeting = remember {"machin"}
                     Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
@@ -69,6 +91,21 @@ fun a() {
 
 
 }
+
+@Composable
+fun RollResult(rolls:List<Int>) {
+
+    Column {
+        rolls.forEach { roll ->
+            Row{
+                Text(" $roll")
+            }
+        }
+
+    }
+}
+
+
 @Composable
 fun DiceSelector(selectedDice: Dice, updateDice: (Dice) -> Unit) {
     val dices = Dice.entries
