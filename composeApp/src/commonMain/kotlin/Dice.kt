d1d9d04874ff031fdd2e615c55f3659b66d1ec01
Adel <576853-astran@users.noreply.gitlab.com>
2024-07-10 23:03:23 +0200

    wip

---
+++ composeApp/src/commonMain/kotlin/Dice.kt
@@ -0,0 +1,17 @@
+import kotlin.random.Random
+
+enum class Dice(private val values: List<Int>) {
+    TWO(listOf(1,2)),
+    FOUR(listOf(1,2,3,4)),
+    SIX((1..6).toList()),
+    TEN((1..10).toList()),
+    TWELVE((1..12).toList()),
+    TWENTY((1..20).toList());
+
+    private fun randomValue(): Int = values[Random.nextInt(values.size)]
+    fun roll( rolls:Int=1 ):List<Int> = (0 until  rolls).map { randomValue() }
+}
+
+fun main() {
+    println(Dice.TWO.roll(5))
+}
