356c1e8c5d88b4c671bcc900b1dc115afef32247
Adel <576853-astran@users.noreply.gitlab.com>
2024-07-10 23:46:05 +0200

    wip

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,7 +1,6 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -14,11 +13,13 @@ import androidx.compose.material3.*
 import diceroller.composeapp.generated.resources.Res
 import diceroller.composeapp.generated.resources.compose_multiplatform
 
+
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        var (selectedDice ,updateDice) = remember { mutableStateOf(Dice.SIX) }
         a()
 
         PermanentNavigationDrawer(
@@ -26,9 +27,10 @@ fun App() {
                 ModalDrawerSheet {
 
                     Column {
-                        Button(onClick = { println("machin") }) { Text("truc") }
-                        Button(onClick = { println("machin2") }) { Text("truc2") }
-                        Button(onClick = { println("machin3") }) { Text("truc3") }
+//                        Button(onClick = { println("machin") }) { Text("truc") }
+//                        Button(onClick = { println("machin2") }) { Text("truc2") }
+//                        Button(onClick = { println("machin3") }) { Text("truc3") }
+                        DiceSelector(selectedDice ,updateDice)
                     }
                 }
             },
@@ -36,7 +38,7 @@ fun App() {
             Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                 Button(onClick = { showContent = !showContent }) {
 
-                    Text("Click mex!")
+                    Text("Selected $selectedDice")
                 }
                 AnimatedVisibility(showContent) {
                     val greeting = remember {"machin"}
@@ -66,4 +68,62 @@ fun a() {
 
 
 
+}
+@Composable
+fun DiceSelector(selectedDice: Dice, updateDice: (Dice) -> Unit) {
+    val dices = Dice.entries
+
+    Column {
+        dices.forEach { dice ->
+            Row{
+                Button(onClick = { updateDice( dice) }) { Text(dice.name) }
+            }
+        }
+        Text("Selected $selectedDice")
+    }
+}
+
+@Composable
+fun Demo_DropDownMenu() {
+
+    var expanded by remember { mutableStateOf(true) }
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false }
+    ) {
+        DropdownMenuItem(
+            text = { Text("Load") },
+            onClick = { println("click load") }
+        )
+        DropdownMenuItem(
+            text = { Text("Save") },
+            onClick = { println("click save") }
+        )
+    }
+//
+//    Box(
+//        modifier = Modifier.fillMaxWidth()
+//            .wrapContentSize(Alignment.TopEnd)
+//    ) {
+//        IconButton(onClick = { expanded = !expanded }) {
+//            Icon(
+//                imageVector = Icons.Default.MoreVert,
+//                contentDescription = "More"
+//            )
+//        }
+//
+//        DropdownMenu(
+//            expanded = expanded,
+//            onDismissRequest = { expanded = false }
+//        ) {
+//            DropdownMenuItem(
+//                text = { Text("Load") },
+//                onClick = { println("click load") }
+//            )
+//            DropdownMenuItem(
+//                text = { Text("Save") },
+//                onClick = { println("click save") }
+//            )
+//        }
+//    }
 }
